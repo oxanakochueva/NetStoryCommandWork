fa9171df74e9b69718dd83322ad9ec76a3b2f235
Oxana Kochueva <oxanak99@gmail.com>
2018-11-18 22:24:04 +0300

    +

---
+++ app/views/application/_menubar.html.rb
@@ -0,0 +1,9 @@
+<header class="menubar">
+    <div>
+    <%= link_to 'Designs', root_path %>
+    <%= link_to 'Attachmennts', attachmennts_path %>
+    <%= link_to 'Articles', articles_path %>
+    <%= link_to 'Spams', spams_path %>
+    </div>
+
+</header>
