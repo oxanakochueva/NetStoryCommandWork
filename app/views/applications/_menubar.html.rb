59273d3f0103f184123490c9ed097e8ac3c22800
anniekotenkova <31893553+anniekotenkova@users.noreply.github.com>
2018-11-19 01:34:33 +0300

    Styles + Articles + Menu

---
+++ app/views/applications/_menubar.html.rb
@@ -0,0 +1,5 @@
+  <div class="menubar">
+    <%= link_to 'Articles', root_path %>
+    <%= link_to 'Attachmennts', attachmennts_path %>
+    <%= link_to 'Spam', spams_path %>
+  </div>
--- db/migrate/20181117212413_create_articles.rb
+++ db/migrate/20181117212413_create_articles.rb
@@ -1,11 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-      # t.integer :design_id
+      t.integer :design_id
       t.text :title
       t.text :body
-      # t.string :cover
-      # t.integer :year_id
+      t.string :cover
+      t.integer :year_id
+      t.boolean :published
 
       t.timestamps
     end
